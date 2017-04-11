545fdb4b10c26e35aa3641e1d09e45d9ad3c4c93
petermccloud <peter@mccloudaero.com>
2017-04-11 09:37:48 -0700

    minor updates to rpm sensor arduino sketch

--- avionics/rpm/rpm_sensor/rpm_sensor.ino
+++ avionics/rpm/rpm_sensor/rpm_sensor.ino
@@ -1,5 +1,5 @@
 /*
-Arduino RPM Sensor
+Arduino RPM Sensor based on Trinket Pro 5V
 by
 Peter McCloud - McCloud Aero Corp (http://mccloudaero.com)
 
@@ -8,14 +8,16 @@ Hall Effect Sensor Project on DIY Hacking (http://diyhacking.com)
 by Arvind Sanjeev
 */
 
+ const bool debug_mode = false;
 
  volatile byte revolutions;
  unsigned int rpm;
  unsigned long timeold;
+ 
  void setup()
  {
    Serial.begin(115200);
-   // Pins 2 & 3 are available for interupts, Pin 2 is used by USB on Trinket Pro
+   // Trinket Pro: Pins 2 & 3 are available for interupts, Pin 2 is used by USB
    // 0 - Pin 2
    // 1 = Pin 3
    attachInterrupt(1, magnet_detect, RISING);//Initialize the intterrupt pin (Arduino digital pin 2)
@@ -35,11 +37,11 @@ by Arvind Sanjeev
  void magnet_detect()//This function is called whenever a magnet/interrupt is detected by the arduino
  {
    revolutions++;
-   // All of the following is for debugging only
-   Serial.println("detect");
-   // Blink the LED on Interupt
-   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-   delay(1000);                       // wait for a second
-   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
+   if (debug_mode == true) {
+     Serial.println("detect");
+     // Blink the LED on Interupt
+     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+     delay(1000);                       // wait for a second
+     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+   }
  }
